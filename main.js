a10dd6c1e5f5c805cbef90eeaf05074d76f4da5a
namanvikas <79072465+namanvikas@users.noreply.github.com>
2021-08-22 12:04:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
+object="";
+status=[];
 
 function setup(){
     canvas=createCanvas(480,380);
@@ -14,8 +15,30 @@ function start(){
 }
 function draw(){
     image(video,0,0,480,380);
-    document.getElementById("Id_of_the_input_box").value;
+    if(Status!=""){
+        if(object[i].label == object_name){
+            document.getElementById("status").innerHTML="Status:object detected";
+            document.getElementById("number_of_object").innerHTML="mumber_of_object:"+object.length
+            
+             document.getElementById("Id_of_the_input_box").value;
+             fill("#ff0000");
+             percante=floor(object[i].confidence*100)
+            text(object[i].label+""+percante+"%",object[i].x+15,object[i].y+15);
+            nofill();
+            stroke("#ff0000");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
 }
+    }
+}
+function gotresults(error,results){
+    if(error){
+        console.error(error)
+}
+console.log(results);
+object=results
+}
+
+
 function modelloaded(){
     console.log("model loaded");
     status=true;
